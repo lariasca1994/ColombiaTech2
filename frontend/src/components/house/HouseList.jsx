7d76f113bff002295dc3517125628d7833f70e66
Carraian <49419170+lariasca1994@users.noreply.github.com>
2026-09-19 16:32:12 +0100

    fix: estructura de celdas Avatar y Actions en HouseList

--- frontend/src/components/house/HouseList.jsx
+++ frontend/src/components/house/HouseList.jsx
@@ -65,17 +65,18 @@ export default function HouseList(){
                         <img className="size-40 transition-transform duration-1000 transform hover:scale-110 max-w-none"
                             src={house.image}
                         />
-                    <td className="border-y-2 px-4 py-2 border-indigo-600"></td>
-                    <div className="inline-flex rounded-md shadow-sm" role="group">
-                        <Link to={`/house/${house.code}`}
-                                className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border
-                                             border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white
-                                             focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white
-                                             dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">Edit</Link>
-                        <button type="button" onClick={()=>{handleDelete(house)}}className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-                            Delete
-                        </button>
-                    </div>
+                    </td>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">
+                        <div className="inline-flex rounded-md shadow-sm" role="group">
+                            <Link to={`/house/${house.code}`}
+                                    className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border
+                                                 border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white
+                                                 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white
+                                                 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">Edit</Link>
+                            <button type="button" onClick={()=>{handleDelete(house)}}className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                Delete
+                            </button>
+                        </div>
                     </td>
                 </tr>
                 ))}
